4e5d6ff7f0c2d8aac09fce4a1795c2ca2924283a
Calin Ennis <calinennis@users.noreply.github.com>
2018-06-13 14:51:37 -0400

    app title shorter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Statefull UI With NgRx Store and Effects';
+  title = 'Stateful UI With NgRx!';
 
   constructor() {}
 }
